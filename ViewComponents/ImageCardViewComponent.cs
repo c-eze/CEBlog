d7e7b4af90e3b6e646618dbc8dd17f6c90f78a16
c-eze <chikere.ezekannagha@gmail.com>
2024-08-27 23:10:15 -0500

    Update Sidebar components

---
+++ ViewComponents/ImageCardViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using CEBlog.Data;
+using CEBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace CEBlog.ViewComponents
+{
+    public class ImageCardViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ImageCardViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Post imagePost)
+        { 
+            return View("Default", imagePost);
+        }
+    }
+}
