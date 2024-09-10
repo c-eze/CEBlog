8f6501fd7bc62cdfc065e1574042a590971309b3
c-eze <chikere.ezekannagha@gmail.com>
2024-09-10 18:02:36 -0500

    Update website footer

---
+++ ViewComponents/RecentPostViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using CEBlog.Data;
+using CEBlog.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CEBlog.ViewComponents
+{
+    public class RecentPostViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecentPostViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var blogs = _context.Posts
+                .Include(b => b.Blog)
+                .Where(p => p.ReadyStatus == ReadyStatus.ProductionReady)
+                .OrderByDescending(p => p.Created)
+                .Take(2);
+
+            return View("Default", await blogs.ToListAsync());
+
+        }
+    }
+}
