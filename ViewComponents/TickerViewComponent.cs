abaf4f0e785cab213cc2466a70387d65d659973f
c-eze <chikere.ezekannagha@gmail.com>
2024-09-26 09:08:53 -0500

    Added trending ticker

---
+++ ViewComponents/TickerViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using CEBlog.Data;
+using CEBlog.Models;
+using CEBlog.ViewModels; 
+using Microsoft.AspNetCore.Mvc;
+
+namespace CEBlog.ViewComponents
+{
+    public class TickerViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TickerViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = _context.Posts
+                        .OrderByDescending(p => p.Created)
+                        .Take(3)
+                        .ToList();
+
+            List<TickerViewModel> tickerVM = new List<TickerViewModel>();
+
+            foreach (Post post in posts) 
+            {
+                tickerVM.Add(new TickerViewModel()
+                {
+                    Title = post.Title,
+                    Slug = post.Slug
+                });
+            }
+
+            return View("Default", tickerVM);
+        }
+    }
+}
---
+++ ViewModels/TickerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CEBlog.ViewModels
+{
+    public class TickerViewModel
+    {
+        public string? Title { get; set; }
+        public string? Slug { get; set; }
+    }
+}
